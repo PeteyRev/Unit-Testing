9fe7d26c8015510b42c17d599df91f2e1ca535f9
Peter Arevalo <peteywiggin@gmail.com>
2016-05-19 09:53:05 -0500

    first commit

---
+++ test/main-test.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+//Test suite
+
+describe('Mocha', function() {
+    //sanity check
+    it('should run our tests using npm', function () {
+        expect(true).to.be.ok;
+    });
+});
